277eaab4b4e45d3494a9bb2a65db098361eeb570
Pablo Carvalho <pablocarvalho.dev@gmail.com>
2024-04-22 23:11:20 -0300

    fixes

--- app/db/route.py
+++ app/db/route.py
@@ -13,7 +13,7 @@ class CRUDMixin(Model):
         """
         It takes a class and returns a function that takes keyword arguments and returns an instance of the
         class
-        
+
         :param cls: The class that the method is being called on
         :return: The instance of the class.
         """
@@ -23,20 +23,21 @@ class CRUDMixin(Model):
     def update(self, commit=True, **kwargs):
         """
         It takes a dictionary of attributes and values, and updates the model instance with those values
-        
+
         :param commit: If True, then the changes are saved to the database. If False, then they're not,
         defaults to True (optional)
         :return: The object itself.
         """
-        for attr, value in kwargs.items():
-            setattr(self, attr, value)
+        for key, value in kwargs.items():
+            if hasattr(self, key):  # Check if the attribute exists in the instance
+                setattr(self, key, value)
         return commit and self.save() or self
 
     def save(self, commit=True):
         """
         It adds the object to the database, and if the commit parameter is True, it commits the changes to
         the database
-        
+
         :param commit: If True, the changes will be committed to the database. If False, the changes will be
         saved in the session, but not committed to the database until you call db.session.commit(), defaults
         to True (optional)
@@ -51,7 +52,7 @@ class CRUDMixin(Model):
         """
         If the commit parameter is True, then set the deleted_at column to the current time and commit the
         changes to the database
-        
+
         :param commit: If True, the object will be committed to the database. If False, the object will not
         be committed to the database, defaults to True (optional)
         :return: The return value is the result of the expression.
@@ -63,7 +64,7 @@ class CRUDMixin(Model):
         """
         It deletes the object from the database, and if the commit parameter is True, it commits the change
         to the database
-        
+
         :param commit: If True, the session will be committed after the object is deleted, defaults to True
         (optional)
         :return: The commit and db.session.commit()
@@ -77,7 +78,7 @@ class RoutingSession(orm.Session):
     def __init__(self, db, autocommit=False, autoflush=False, **options):
         """
         The function creates a new session object, and binds it to the database engine.
-        
+
         :param db: The SQLAlchemy database object
         :param autocommit: If set to True, all objects will be automatically committed after each flush,
         defaults to False (optional)
@@ -97,12 +98,12 @@ class RoutingSession(orm.Session):
     def get_bind(self, mapper=None, clause=None):
         """
         If you have a bind name, use it. Otherwise, use the default
-        
+
         :param mapper: The mapper associated with the query
         :param clause: The SQL expression being compiled
         :return: The return value is a SQLAlchemy engine object.
         """
-    
+
         try:
             state = get_state(self.app)
         except (AssertionError, AttributeError, TypeError) as err:
@@ -127,7 +128,7 @@ class RoutingSession(orm.Session):
         """
         It creates a new session object, copies all the attributes from the current session object to the
         new session object, and then sets the new session object's bind name to the name passed in
-        
+
         :param name: The name of the database to bind to
         :return: A new RoutingSession object with the same attributes as the original RoutingSession object.
         """
@@ -149,7 +150,7 @@ class RouteSQLAlchemy(SQLAlchemy):
     def create_scoped_session(self, options=None):
         """
         It creates a new session object and binds it to the database engine.
-        
+
         :param options: A dictionary of keyword arguments that will then be sent to the sessionmaker
         callable used to create the session
         :return: A new session object, bound to the database engine.
